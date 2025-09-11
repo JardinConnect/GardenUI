3b1b6977a00afe93d74c1e4e175ba9dab9f569a4
Mohamed AZDOUD <mohamed.azd49@gmail.com>
2025-09-11 16:58:41 +0200

    feat: additional tests on item activation

--- test/organisms/menu_test.dart
+++ test/organisms/menu_test.dart
@@ -26,13 +26,17 @@ void main() {
       expect(find.text('Settings'), findsOneWidget);
     });
 
-    testWidgets('applies correct MenuItemSize for each MenuSize', (tester) async {
+    testWidgets('applies correct MenuItemSize for each MenuSize', (
+      tester,
+    ) async {
       Future<MenuItem> pumpAndGetMenuItem(MenuSize size) async {
         await tester.pumpWidget(
           MaterialApp(
             home: Scaffold(
               body: Menu(
-                items: [MenuItem(icon: Icons.home, label: 'Home', onTap: () {})],
+                items: [
+                  MenuItem(icon: Icons.home, label: 'Home', onTap: () {}),
+                ],
                 bottomItems: [],
                 size: size,
               ),
@@ -53,30 +57,90 @@ void main() {
       expect(lgItem.size, equals(MenuItemSize.lg));
     });
 
-    testWidgets('selects item when tapped and updates active state',
-            (WidgetTester tester) async {
-          await tester.pumpWidget(
-            MaterialApp(
-              home: Scaffold(
-                body: Menu(
-                  items: [
-                    MenuItem(icon: Icons.home, label: 'Home', onTap: () {}),
-                    MenuItem(icon: Icons.search, label: 'Search', onTap: () {}),
-                  ],
-                  bottomItems: [],
-                ),
-              ),
+    testWidgets('selects item when tapped and updates active state', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Menu(
+              items: [
+                MenuItem(icon: Icons.home, label: 'Home', onTap: () {}),
+                MenuItem(icon: Icons.search, label: 'Search', onTap: () {}),
+              ],
+              bottomItems: [],
             ),
-          );
+          ),
+        ),
+      );
 
-          await tester.tap(find.text('Search'));
-          await tester.pumpAndSettle();
+      await tester.tap(find.text('Search'));
+      await tester.pumpAndSettle();
 
-          final searchItem = tester.widget<MenuItem>(find.widgetWithText(MenuItem, 'Search'));
-          expect(searchItem.isActive, true);
+      final searchItem = tester.widget<MenuItem>(
+        find.widgetWithText(MenuItem, 'Search'),
+      );
+      expect(searchItem.isActive, true);
 
-          final homeItem = tester.widget<MenuItem>(find.widgetWithText(MenuItem, 'Home'));
-          expect(homeItem.isActive, false);
-        });
+      final homeItem = tester.widget<MenuItem>(
+        find.widgetWithText(MenuItem, 'Home'),
+      );
+      expect(homeItem.isActive, false);
+    });
+  });
+
+  testWidgets('first MenuItem is active by default', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Menu(
+            items: [
+              MenuItem(icon: Icons.home, label: 'Home', onTap: () {}),
+              MenuItem(icon: Icons.search, label: 'Search', onTap: () {}),
+            ],
+            bottomItems: [],
+          ),
+        ),
+      ),
+    );
+
+    final homeItem = tester.widget<MenuItem>(
+      find.widgetWithText(MenuItem, 'Home'),
+    );
+    final searchItem = tester.widget<MenuItem>(
+      find.widgetWithText(MenuItem, 'Search'),
+    );
+
+    expect(homeItem.isActive, isTrue);
+    expect(searchItem.isActive, isFalse);
+  });
+
+  testWidgets('tapping another MenuItem changes active state', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Menu(
+            items: [
+              MenuItem(icon: Icons.home, label: 'Home', onTap: () {}),
+              MenuItem(icon: Icons.search, label: 'Search', onTap: () {}),
+            ],
+            bottomItems: [],
+          ),
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('Search'));
+    await tester.pumpAndSettle();
+
+    final homeItem = tester.widget<MenuItem>(
+      find.widgetWithText(MenuItem, 'Home'),
+    );
+    final searchItem = tester.widget<MenuItem>(
+      find.widgetWithText(MenuItem, 'Search'),
+    );
+
+    expect(homeItem.isActive, isFalse);
+    expect(searchItem.isActive, isTrue);
   });
 }
