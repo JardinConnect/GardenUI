4fcc253ad5afccab29dbeb5c2f7df60e12c6bcb0
Mohamed Azdoud <mohamed.azd49@gmail.com>
2025-12-04 23:20:05 +0100

    fix(foundation): revert GardenRadius changes

--- lib/ui/foundation/radius/radius_design_system.dart
+++ lib/ui/foundation/radius/radius_design_system.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/widgets.dart';
 
 class GardenRadius {
-  static const radiusSizes = {
-    'xs': 4.0,
-    'sm': 8.0,
-    'md': 12.0,
-    'lg': 16.0,
-    'xl': 20.0,
-  };
+  static const radiusSizes = {'xs': 4.0, 'sm': 8.0, 'md': 12.0, 'lg': 16.0};
 
   static final radiusXs = BorderRadius.circular(radiusSizes['xs']!);
   static final radiusSm = BorderRadius.circular(radiusSizes['sm']!);
   static final radiusMd = BorderRadius.circular(radiusSizes['md']!);
   static final radiusLg = BorderRadius.circular(radiusSizes['lg']!);
-  static final radiusXl = BorderRadius.circular(radiusSizes['xl']!);
 }
